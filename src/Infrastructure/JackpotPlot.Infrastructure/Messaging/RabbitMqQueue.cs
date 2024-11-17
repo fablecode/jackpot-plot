73ef16dfd0e683521ec75c08f962408cacae84c7
fablecode <fablecode@users.noreply.github.com>
2024-11-17 19:42:30 +0100

    Infrastructure : Rabbitmq

---
+++ src/Infrastructure/JackpotPlot.Infrastructure/Messaging/RabbitMqQueue.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Text.Json;
+using JackpotPlot.Domain.Messaging;
+using JackpotPlot.Domain.Settings;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace JackpotPlot.Infrastructure.Messaging;
+
+public class RabbitMqQueue<T>: IQueue<T>
+{
+    private readonly IOptions<RabbitMqSettings> _rabbitMqConfig;
+    private readonly ConnectionFactory _factory;
+
+    public RabbitMqQueue(IOptions<RabbitMqSettings> rabbitMqConfig)
+    {
+        _rabbitMqConfig = rabbitMqConfig;
+        _factory = new ConnectionFactory() { HostName = rabbitMqConfig.Value.Host, Port = rabbitMqConfig.Value.Port, UserName = rabbitMqConfig.Value.Username, Password = rabbitMqConfig.Value.Password };
+    }
+    public async Task Publish(T message, string routingKey)
+    {
+        var jsonMessage = JsonConvert.SerializeObject(message, Formatting.Indented);
+        var body = new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes(jsonMessage));
+
+        using (var connection = await _factory.CreateConnectionAsync())
+        {
+            using (var channel = await connection.CreateChannelAsync())
+            {
+                await channel.BasicPublishAsync(_rabbitMqConfig.Value.Exchange, routingKey, true, body);
+            }
+        }
+    }
+}
