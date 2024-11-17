178724557e5c29ea3afc4ba5e48cd714216ebbbd
fablecode <fablecode@users.noreply.github.com>
2024-11-17 19:40:46 +0100

    Applicaton layer

---
+++ src/Application/JackpotPlot.Application/ApplicationInstaller.cs
@@ -0,0 +1,20 @@
+﻿using JackpotPlot.Domain.Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JackpotPlot.Application;
+
+public static class ApplicationInstaller
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services
+            .AddDomainModels()
+            .AddMediatR(cfg
+                => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+    }
+
+    private static IServiceCollection AddDomainModels(this IServiceCollection services)
+    {
+        return services.AddTransient<EurojackpotJobs>();
+    }
+}
