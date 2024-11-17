14fd502c3b8b9f8a3dac84b226cbc0a5209430f8
fablecode <fablecode@users.noreply.github.com>
2024-11-17 19:44:47 +0100

    Infrastructure installer

---
+++ src/Infrastructure/JackpotPlot.Infrastructure/InfrastructureInstaller.cs
@@ -0,0 +1,34 @@
+﻿using JackpotPlot.Domain.Messaging;
+using JackpotPlot.Domain.Services;
+using JackpotPlot.Infrastructure.Messaging;
+using JackpotPlot.Infrastructure.Services;
+using JackpotPlot.Infrastructure.WebPages;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JackpotPlot.Infrastructure
+{
+    public static class InfrastructureInstaller
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddTransient<IHtmlWebPage, HtmlWebPage>();
+
+            return services
+                .AddDomainServices()
+                .AddMessagingServices();
+        }
+
+        public static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            services.AddTransient<IEurojackpotService, EurojackpotService>();
+
+            return services;
+        }
+        public static IServiceCollection AddMessagingServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IQueue<>), typeof(RabbitMqQueue<>));
+
+            return services;
+        }
+    }
+}
