d39273ef1cfbd154fbf8173038b4771ed411ab28
fablecode <fablecode@users.noreply.github.com>
2024-11-17 22:53:59 +0100

    Lottery.API added.

---
+++ src/Presentation/Services/Lottery.API/Controllers/LotteriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lottery.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LotteriesController : ControllerBase
+{
+    /// <summary>
+    /// Get all lotteries
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    [HttpGet]
+    public IActionResult Get()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Presentation/Services/Lottery.API/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var openApiInfo = new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Lottery API",
+        Description = "The core service for handling all lottery-related data. This includes CRUD (Create, Read, Update, Delete) operations for lotteries, draw events, results, and prize breakdowns.",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    };
+
+    options.SwaggerDoc("v1", openApiInfo);
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(u =>
+    {
+        u.RouteTemplate = "swagger/{documentName}/swagger.json";
+    });
+
+    app.UseSwaggerUI(c =>
+    {
+        c.RoutePrefix = "swagger";
+        c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Lottery API V1");
+    });
+}
+
+app.MapControllers();
+app.UseRouting();
+
+app.Run();
