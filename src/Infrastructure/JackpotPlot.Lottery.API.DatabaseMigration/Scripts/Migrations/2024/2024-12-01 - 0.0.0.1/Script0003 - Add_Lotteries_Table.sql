2fbff9dfbf6ffe31fd906a7d63c2426712954270
fablecode <fablecode@users.noreply.github.com>
2024-12-01 22:13:38 +0100

    Lottery database table

---
+++ src/Infrastructure/JackpotPlot.Lottery.API.DatabaseMigration/Scripts/Migrations/2024/2024-12-01 - 0.0.0.1/Script0003 - Add_Lotteries_Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE lotteries (
+    id SERIAL PRIMARY KEY,             -- Unique identifier for each lottery
+    name VARCHAR(255) NOT NULL,                 -- Name of the lottery (e.g., "Mega Millions")
+    description TEXT,                           -- Description of the lottery
+    draw_frequency VARCHAR(50),                 -- Frequency of the draw (e.g., "Weekly", "Daily")
+    status VARCHAR(50) DEFAULT 'active',       -- Status of the lottery (e.g., 'active', 'inactive')
+    created_at TIMESTAMPTZ DEFAULT NOW(),      -- Timestamp when the lottery was created
+    updated_at TIMESTAMPTZ DEFAULT NOW()       -- Timestamp when the lottery information was last updated
+);
