89c36a36f53e18e45cb002ce0a431c3c354b2ac5
fablecode <fablecode@users.noreply.github.com>
2024-12-01 22:14:28 +0100

    Lottery countries database table

---
+++ src/Infrastructure/JackpotPlot.Lottery.API.DatabaseMigration/Scripts/Migrations/2024/2024-12-01 - 0.0.0.1/Script0006 - Add_LotteryCountries_Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE lotteries_countries (
+    lottery_id INT REFERENCES lotteries(id) ON DELETE CASCADE,  -- Associated lottery
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE,  -- Associated country
+    PRIMARY KEY (lottery_id, country_id),                               -- Composite primary key
+    created_at TIMESTAMPTZ DEFAULT NOW(),                               -- Timestamp of creation
+    updated_at TIMESTAMPTZ DEFAULT NOW()                                -- Timestamp of the last update
+);
