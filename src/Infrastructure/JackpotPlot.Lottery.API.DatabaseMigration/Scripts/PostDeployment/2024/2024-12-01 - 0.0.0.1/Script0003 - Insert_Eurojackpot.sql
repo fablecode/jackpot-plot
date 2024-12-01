a5105f42f4e905d146b2fbd5e6f9ce6e8032e6f9
fablecode <fablecode@users.noreply.github.com>
2024-12-01 22:14:54 +0100

    insert Eurojackpot lottery record

---
+++ src/Infrastructure/JackpotPlot.Lottery.API.DatabaseMigration/Scripts/PostDeployment/2024/2024-12-01 - 0.0.0.1/Script0003 - Insert_Eurojackpot.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO lotteries (name, description, draw_frequency, status, created_at, updated_at)
+    VALUES
+        (
+            'EuroJackpot',
+            'EuroJackpot is a transnational European lottery with a 5/50 + 2/10 format. Draws occur weekly, offering large jackpots.',
+            'Weekly',
+            'active',
+            NOW(),
+            NOW()
+        )
