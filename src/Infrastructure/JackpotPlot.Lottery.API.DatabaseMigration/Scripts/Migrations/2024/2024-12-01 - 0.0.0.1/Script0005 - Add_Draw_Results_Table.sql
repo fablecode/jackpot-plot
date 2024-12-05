f123562516c669e3a575f1c339dd4b9f9bacbde9
fablecode <fablecode@users.noreply.github.com>
2024-12-05 12:56:01 +0100

    Renamed Lottery_Numbers sql table to Draw_Results

--- src/Infrastructure/JackpotPlot.Lottery.API.DatabaseMigration/Scripts/Migrations/2024/2024-12-01 - 0.0.0.1/Script0005 - Add_Draw_Results_Table.sql
+++ src/Infrastructure/JackpotPlot.Lottery.API.DatabaseMigration/Scripts/Migrations/2024/2024-12-01 - 0.0.0.1/Script0005 - Add_Draw_Results_Table.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE lottery_numbers (
+﻿CREATE TABLE draw_results (
     id SERIAL PRIMARY KEY,      -- Unique identifier for the drawn number set
     draw_id INT REFERENCES draws(id) ON DELETE CASCADE,  -- The draw this number set belongs to
     numbers INT[] NOT NULL,                    -- Array of drawn numbers (can be adjusted for different lottery formats)
