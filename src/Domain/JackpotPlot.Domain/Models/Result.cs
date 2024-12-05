af1f43a8cf5d7680d3a07d2a231b2b1589bd14ed
fablecode <fablecode@users.noreply.github.com>
2024-12-05 23:12:45 +0100

    Result pattern

---
+++ src/Domain/JackpotPlot.Domain/Models/Result.cs
@@ -0,0 +1,42 @@
+﻿namespace JackpotPlot.Domain.Models;
+
+public record Result<T>
+{
+    public bool IsSuccess { get; }
+    public T Value { get; }
+    public string[] Errors { get; }
+    public bool IsFailure => !IsSuccess;
+
+    private Result(T value, bool isSuccess, params string[] errors)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Errors = errors;
+    }
+
+    public static Result<T> Success(T value) => new(value, true, null!);
+    public static Result<T> Failure(params string[] error) => new(default!, false, error);
+
+    // OnSuccess method for chaining operations
+    public Result<TOutput> OnSuccess<TOutput>(Func<T, Result<TOutput>> func)
+    {
+        return !IsSuccess ? Result<TOutput>.Failure(Errors) : func(Value);
+    }
+
+    public Result<T> OnSuccess(Action<T> action)
+    {
+        if (IsSuccess)
+        {
+            action(Value);
+        }
+
+        return this;
+    }
+
+    // OnFailure method to handle errors
+    public Result<T> OnFailure(Action<string[]> action)
+    {
+        if (!IsSuccess) action(Errors);
+        return this;
+    }
+}
