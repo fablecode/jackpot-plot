339895379a8afd6fcd803ddf98f362e88ab1f7de
fablecode <fablecode@users.noreply.github.com>
2024-12-09 17:32:00 +0100

    GetJackpotAmount to sanatize Jackpot value

--- src/Domain/JackpotPlot.Domain/Models/EurojackpotResult.cs
+++ src/Domain/JackpotPlot.Domain/Models/EurojackpotResult.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Immutable;
 using System.Data;
+using System.Globalization;
 
 namespace JackpotPlot.Domain.Models;
 
@@ -13,4 +14,29 @@ public class EurojackpotResult
     public int JackpotWinners { get; set; }
     public string JackpotAmount { get; set; }
     public ImmutableArray<DataTable> PrizeBreakdown { get; set; }
+
+    public decimal GetJackpotAmount()
+    {
+        if (string.IsNullOrWhiteSpace(JackpotAmount))
+        {
+            return default;
+        }
+
+        // Extract the numeric part and the prefix dynamically
+        var index = 0;
+
+        // Identify where the numeric part begins
+        while (index < JackpotAmount.Length && !char.IsDigit(JackpotAmount[index]))
+        {
+            index++;
+        }
+
+        // Extract the numeric part
+        var numericPart = JackpotAmount.Substring(index);
+
+        // Remove commas from the numeric part
+        numericPart = numericPart.Replace(",", "");
+
+        return decimal.Parse(numericPart, NumberStyles.AllowThousands);
+    }
 }
