b8024293053fd39e28203cd39fc9cdc5942bf61a
fablecode <fablecode@users.noreply.github.com>
2024-12-15 15:07:02 +0100

    JackpotPlot.Prediction.API.Application project added.

---
+++ src/Application/JackpotPlot.Prediction.API.Application/ApplicationInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JackpotPlot.Prediction.API.Application;
+
+public static class ApplicationInstaller
+{
+    public static IServiceCollection AddPredictionApiApplicationServices(this IServiceCollection services)
+    {
+        return services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+    }
+
+}
---
+++ src/Application/JackpotPlot.Prediction.API.Application/Messaging/Handlers/MessageHandler.cs
@@ -0,0 +1,6 @@
+﻿using JackpotPlot.Domain.Models;
+using MediatR;
+
+namespace JackpotPlot.Prediction.API.Application.Messaging.Handlers;
+
+public record MessageHandler<T> (T Message) : IRequest<Result<T>>;
