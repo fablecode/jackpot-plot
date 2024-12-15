ac3aaaa40d0620815ee1cbd9ad73e94d7d34b3ae
fablecode <fablecode@users.noreply.github.com>
2024-12-15 15:08:57 +0100

    Lottery drawn hosted service

---
+++ src/Presentation/Services/Prediction.API/HostedServices/LotteryDrawnBackgroundService.cs
@@ -0,0 +1,24 @@
+﻿using JackpotPlot.Domain.Constants;
+using JackpotPlot.Domain.Messaging;
+using MediatR;
+
+namespace Prediction.API.HostedServices;
+
+public class LotteryDrawnBackgroundService<T> : RabbitMqBackgroundService<T>
+{
+
+    public LotteryDrawnBackgroundService
+    (
+        ILogger<LotteryDrawnBackgroundService<T>> logger, 
+        IQueueReader<T> queueReader,
+        IMediator mediator) 
+        : base(logger, mediator, queueReader)
+    {
+    }
+
+    protected override string QueueName => "lottery-db-update";
+    protected override HashSet<string> RoutingKeys =>
+    [
+        string.Join('.', JackpotPlot.Domain.Constants.RoutingKeys.LotteryDbUpdate, EventTypes.LotteryDrawn)
+    ];
+}
---
+++ src/Presentation/Services/Prediction.API/HostedServices/RabbitMqBackgroundService.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Channels;
+using JackpotPlot.Domain.Messaging;
+using JackpotPlot.Domain.Models;
+using JackpotPlot.Prediction.API.Application.Messaging.Handlers;
+using MediatR;
+
+namespace Prediction.API.HostedServices;
+
+public abstract class RabbitMqBackgroundService<T> : BackgroundService
+{
+    protected abstract string QueueName { get; }
+    protected abstract HashSet<string> RoutingKeys { get; }
+
+    private readonly ILogger<RabbitMqBackgroundService<T>> _logger;
+    private readonly IMediator _mediator;
+    private readonly IQueueReader<T> _queueReader;
+
+    protected RabbitMqBackgroundService(ILogger<RabbitMqBackgroundService<T>> logger, IMediator mediator, IQueueReader<T> queueReader)
+    {
+        _logger = logger;
+        _mediator = mediator;
+        _queueReader = queueReader;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("RabbitMq Background Service for {Queue} is starting.", QueueName);
+
+        // Create a channel to receive messages
+        var channel = Channel.CreateUnbounded<(T, TaskCompletionSource<Result<T>>)>();
+
+        // Start reading messages asynchronously
+        var readingTask = _queueReader.Subscribe(QueueName, RoutingKeys, channel, stoppingToken);
+
+        // Process messages as they come in
+        await foreach (var (message, ack) in channel.Reader.ReadAllAsync(stoppingToken))
+        {
+            _logger.LogInformation("Message received from queue {Queue}.", QueueName);
+
+            // Process the message (this can be async)
+            var result = await _mediator.Send(new MessageHandler<T>(message), stoppingToken);
+
+            ack.SetResult(result);
+        }
+
+        // Await the completion of reading messages
+        await readingTask;
+
+        _logger.LogInformation("RabbitMq Background Service for {Queue} is stopping.", QueueName);
+    }
+}
