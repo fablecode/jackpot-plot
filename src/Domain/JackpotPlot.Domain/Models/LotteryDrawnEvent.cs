0813f547d47f35d8225eded677006832b4594e42
fablecode <fablecode@users.noreply.github.com>
2024-12-15 11:07:01 +0100

    Lottery drawn event model

---
+++ src/Domain/JackpotPlot.Domain/Models/LotteryDrawnEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Immutable;
+
+namespace JackpotPlot.Domain.Models;
+
+public sealed class LotteryDrawnEvent
+{
+    public int LotteryId { get; set; }
+    public DateTime DrawDate { get; set; }
+    public ImmutableArray<int> WinningNumbers { get; set; }
+    public ImmutableArray<int> BonusNumbers { get; set; }
+}
