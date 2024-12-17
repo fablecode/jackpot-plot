c8fe9a6b036c85b885e82da036140039714c804d
fablecode <fablecode@users.noreply.github.com>
2024-12-17 16:25:14 +0100

    Prediction strategy types

---
+++ src/Domain/JackpotPlot.Domain/Constants/PredictionStrategyType.cs
@@ -0,0 +1,8 @@
+﻿namespace JackpotPlot.Domain.Constants;
+
+public static class PredictionStrategyType
+{
+    public const string Random = "random";
+    public const string FrequencyBased = "frequency-based";
+    public const string AiBased = "ai-based";
+}
