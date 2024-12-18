ac964d1b79f89a59df6231d0e1c923ec0a4324eb
fablecode <fablecode@users.noreply.github.com>
2024-12-18 23:23:58 +0100

    GetHistoricalDraws method

--- src/Domain/JackpotPlot.Domain/Repositories/ILotteryHistoryRepository.cs
+++ src/Domain/JackpotPlot.Domain/Repositories/ILotteryHistoryRepository.cs
@@ -1,8 +1,10 @@
-﻿using JackpotPlot.Domain.Models;
+﻿using System.Collections.Immutable;
+using JackpotPlot.Domain.Models;
 
 namespace JackpotPlot.Domain.Repositories;
 
 public interface ILotteryHistoryRepository
 {
     Task<int> Add(LotteryDrawnEvent lotteryDrawnEvent);
+    Task<ICollection<HistoricalDraw>> GetHistoricalDraws(int lotteryId);
 }
