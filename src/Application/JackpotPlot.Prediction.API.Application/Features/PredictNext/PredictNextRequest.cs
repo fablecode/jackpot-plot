5774cff67a14747ead8daa846e925b617a5988a3
fablecode <fablecode@users.noreply.github.com>
2024-12-18 23:24:50 +0100

    Prediction next request

---
+++ src/Application/JackpotPlot.Prediction.API.Application/Features/PredictNext/PredictNextRequest.cs
@@ -0,0 +1,7 @@
+﻿using JackpotPlot.Domain.Constants;
+using JackpotPlot.Domain.Models;
+using MediatR;
+
+namespace JackpotPlot.Prediction.API.Application.Features.PredictNext;
+
+public record PredictNextRequest(int LotteryId, int? UserId = null, string Strategy = PredictionStrategyType.Random) : IRequest<Result<int>>;
---
+++ src/Application/JackpotPlot.Prediction.API.Application/Features/PredictNext/PredictNextRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using JackpotPlot.Domain.Interfaces;
+using JackpotPlot.Domain.Models;
+using JackpotPlot.Domain.Repositories;
+using MediatR;
+
+namespace JackpotPlot.Prediction.API.Application.Features.PredictNext;
+
+public sealed class PredictNextRequestHandler : IRequestHandler<PredictNextRequest, Result<int>>
+{
+    private readonly IEnumerable<IPredictionStrategy> _predictionStrategies;
+    private readonly IPredictionRepository _predictionRepository;
+
+    public PredictNextRequestHandler(IEnumerable<IPredictionStrategy> predictionStrategies, IPredictionRepository predictionRepository)
+    {
+        _predictionStrategies = predictionStrategies;
+        _predictionRepository = predictionRepository;
+    }
+    public async Task<Result<int>> Handle(PredictNextRequest request, CancellationToken cancellationToken)
+    {
+        var predictionStrategy = _predictionStrategies.Single(ps => ps.Handles(request.Strategy));
+
+        var predictionResult = await predictionStrategy.Predict(request.LotteryId);
+
+        if (predictionResult.IsSuccess)
+        {
+            var predictionId = await _predictionRepository.Add(predictionResult.Value);
+            return Result<int>.Success(predictionId);
+        }
+
+        return Result<int>.Failure(predictionResult.Errors);
+    }
+}
