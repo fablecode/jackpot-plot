392dbc138e1396fddc53214b88807c0ebc4b4ef1
fablecode <fablecode@users.noreply.github.com>
2024-12-18 23:24:13 +0100

    IPredictioniRepository

---
+++ src/Domain/JackpotPlot.Domain/Repositories/IPredictionRepository.cs
@@ -0,0 +1,8 @@
+﻿using JackpotPlot.Domain.ValueObjects;
+
+namespace JackpotPlot.Domain.Repositories;
+
+public interface IPredictionRepository
+{
+    Task<int> Add(PredictionResult predictionResult);
+}
