b6c4a69f5a0392876fc025c88204f4176ab3b251
fablecode <fablecode@users.noreply.github.com>
2025-01-04 23:24:43 +0100

    Updated Application routes

--- src/Presentation/Clients/Web/jackpot-plot-web/src/app/app.routes.ts
+++ src/Presentation/Clients/Web/jackpot-plot-web/src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {LayoutComponent} from './layout/layout.component';
+import {HomeComponent} from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: LayoutComponent,
+        children: [
+          { path: '', component: HomeComponent }
+        ]
+      },
+      { path: 'home', component: HomeComponent },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
