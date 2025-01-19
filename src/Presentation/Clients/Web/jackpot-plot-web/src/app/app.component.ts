f2df35d3730c7afaa015760b69dbd247a3585f7c
fablecode <fablecode@users.noreply.github.com>
2025-01-19 08:13:50 +0100

    jackpot plot initial metronic working version

--- src/Presentation/Clients/Web/jackpot-plot-web/src/app/app.component.ts
+++ src/Presentation/Clients/Web/jackpot-plot-web/src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { TranslationService } from './modules/i18n';
+// language list
+import { locale as enLang } from './modules/i18n/vocabs/en';
+import { locale as chLang } from './modules/i18n/vocabs/ch';
+import { locale as esLang } from './modules/i18n/vocabs/es';
+import { locale as jpLang } from './modules/i18n/vocabs/jp';
+import { locale as deLang } from './modules/i18n/vocabs/de';
+import { locale as frLang } from './modules/i18n/vocabs/fr';
+import { ThemeModeService } from './_metronic/partials/layout/theme-mode-switcher/theme-mode.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  // tslint:disable-next-line:component-selector
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'body[root]',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'jackpot-plot-web';
+export class AppComponent implements OnInit {
+  constructor(
+    private translationService: TranslationService,
+    private modeService: ThemeModeService
+  ) {
+    // register translations
+    this.translationService.loadTranslations(
+      enLang,
+      chLang,
+      esLang,
+      jpLang,
+      deLang,
+      frLang
+    );
+  }
+
+  ngOnInit() {
+    this.modeService.init();
+  }
 }
