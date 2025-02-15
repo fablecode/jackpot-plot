6fbd25a4808e8eea87e144cc17e09e3d4b4d7565
fablecode <fablecode@users.noreply.github.com>
2025-02-15 11:49:58 +0100

    Removed unused import statement

--- src/Presentation/Clients/jackpot-plot-web/src/app/app.routes.ts
+++ src/Presentation/Clients/jackpot-plot-web/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import {IndexComponent} from './pages/index/index.component';
 import {dashboardRoutes} from './features/dashboard/dashboard.routes';
 import {ROUTE_PATHS} from './core/constants/routes.constants';
 
