8f67fa38db6aa353e1b99997d64627887637ade6
fablecode <fablecode@users.noreply.github.com>
2025-02-16 22:02:42 +0100

    Get lotteries feature

---
+++ src/Application/JackpotPlot.Lottery.API.Application/Features/GetLotteries/GetLotteriesQuery.cs
@@ -0,0 +1,7 @@
+﻿using JackpotPlot.Domain.Domain;
+using JackpotPlot.Domain.Models;
+using MediatR;
+
+namespace JackpotPlot.Lottery.API.Application.Features.GetLotteries;
+
+public record GetLotteriesQuery: IRequest<Result<ICollection<LotteryDomain>>>;
---
+++ src/Application/JackpotPlot.Lottery.API.Application/Features/GetLotteries/GetLotteriesQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using JackpotPlot.Domain.Domain;
+using JackpotPlot.Domain.Models;
+using JackpotPlot.Domain.Repositories;
+using MediatR;
+
+namespace JackpotPlot.Lottery.API.Application.Features.GetLotteries;
+
+public sealed class GetLotteriesQueryHandler : IRequestHandler<GetLotteriesQuery, Result<ICollection<LotteryDomain>>>
+{
+    private readonly ILotteryRepository _lotteryRepository;
+
+    public GetLotteriesQueryHandler(ILotteryRepository lotteryRepository)
+    {
+        _lotteryRepository = lotteryRepository;
+    }
+
+    public async Task<Result<ICollection<LotteryDomain>>> Handle(GetLotteriesQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _lotteryRepository.GetLotteries();
+
+        return Result<ICollection<LotteryDomain>>.Success(result);
+    }
+}
