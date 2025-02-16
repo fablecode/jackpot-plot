4cb37be126e26f6e5d93ce98e4d952634ddfb33d
fablecode <fablecode@users.noreply.github.com>
2025-02-16 22:05:10 +0100

    Number generator UI screen.

--- src/Presentation/Clients/jackpot-plot-web/src/app/features/predictions/pages/number-generator/number-generator.component.ts
+++ src/Presentation/Clients/jackpot-plot-web/src/app/features/predictions/pages/number-generator/number-generator.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LotteryService} from '../../../../core/services/lottery.service';
+import {Lottery} from '../../../../core/models/lottery.model';
+import {FormsModule} from '@angular/forms';
+import {CommonModule, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-number-generator',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './number-generator.component.html',
   styleUrl: './number-generator.component.scss'
 })
-export class NumberGeneratorComponent {
+export class NumberGeneratorComponent implements OnInit {
+  lotteries: Lottery[] = []; // Now using the `Lottery` model
 
+  selectedLottery: number | null = null;
+  loading = true; // Flag for loading state
+
+  constructor(private lotteryService: LotteryService) {}
+
+  onLotteryChange(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.selectedLottery = Number(selectedValue);
+    console.log('Selected Lottery ID:', this.selectedLottery);
+  }
+
+  ngOnInit(): void {
+      this.lotteryService.getAllLotteries().subscribe({
+        next: (data: Lottery[]) => {
+          this.lotteries = data;
+          this.loading = false;
+        },
+        error: (err) => console.error('Error fetching lotteries:', err)
+      });
+    }
 }
