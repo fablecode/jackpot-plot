46bbd75467d79898853f24c2f8cca5ee51f0885e
fablecode <fablecode@users.noreply.github.com>
2025-02-17 23:14:52 +0100

    Prediction strategy attribute added.

---
+++ src/Domain/JackpotPlot.Domain/Services/PredictionStrategies/Attributes/PredictionStrategyDescriptionAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace JackpotPlot.Domain.Services.PredictionStrategies.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public sealed class PredictionStrategyDescriptionAttribute(string id, string description) : Attribute
+{
+    public string Id { get; init; } = id;
+    public string Description { get; init; } = description;
+}
