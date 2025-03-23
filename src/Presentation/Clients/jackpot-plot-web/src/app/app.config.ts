dc063b302d5b097c7c061f9569813dcaaab98f6b
fablecode <fablecode@users.noreply.github.com>
2025-03-23 21:33:50 +0100

    Keycloak packages

--- src/Presentation/Clients/jackpot-plot-web/src/app/app.config.ts
+++ src/Presentation/Clients/jackpot-plot-web/src/app/app.config.ts
@@ -3,11 +3,24 @@ import {provideRouter, withComponentInputBinding} from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import {provideKeycloak} from 'keycloak-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding())
+    provideRouter(routes, withComponentInputBinding()),
+    provideKeycloak({
+      config: {
+        // url: 'http://keycloak:8080/auth',
+        url: 'http://localhost:8085',
+        realm: 'jackpotplot',
+        clientId: 'jackpotplot-web',
+      },
+      initOptions: {
+        onLoad: 'check-sso', // or 'login-required' based on your needs
+        silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+      },
+    })
   ]
 };
