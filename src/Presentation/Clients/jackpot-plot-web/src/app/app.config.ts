36f4aa113cd9bba8a298f3a69d388ee1647d4816
fablecode <fablecode@users.noreply.github.com>
2025-04-02 10:37:34 +0200

    user Tools Routes

--- src/Presentation/Clients/jackpot-plot-web/src/app/app.config.ts
+++ src/Presentation/Clients/jackpot-plot-web/src/app/app.config.ts
@@ -3,46 +3,7 @@ import {provideRouter, withComponentInputBinding} from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
-import {
-  AutoRefreshTokenService,
-  createInterceptorCondition, INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
-  IncludeBearerTokenCondition,
-  provideKeycloak, UserActivityService,
-  withAutoRefreshToken
-} from 'keycloak-angular';
-
-const localhostCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
-  urlPattern: /^(http:\/\/localhost:8085)(\/.*)?$/i // Match URLs starting with http://localhost:8181
-});
-
-export const provideKeycloakAngular = () =>
-  provideKeycloak({
-    config: {
-      // url: 'http://keycloak:8080/auth',
-      url: 'http://localhost:8085',
-      realm: 'jackpotplot',
-      clientId: 'jackpotplot-web',
-    },
-    initOptions: {
-      onLoad: 'check-sso',
-      silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
-      redirectUri: window.location.origin + '/'
-    },
-    features: [
-      withAutoRefreshToken({
-        onInactivityTimeout: 'logout',
-        sessionTimeout: 60000
-      })
-    ],
-    providers: [
-      AutoRefreshTokenService,
-      UserActivityService,
-      {
-        provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
-        useValue: [localhostCondition] // Specify conditions for adding the Bearer token
-      }
-    ]
-  });
+import {provideKeycloakAngular} from './keycloak.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- src/Presentation/Clients/jackpot-plot-web/src/app/app.routes.ts
+++ src/Presentation/Clients/jackpot-plot-web/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {dashboardRoutes} from './features/dashboard/dashboard.routes';
 import {ROUTE_PATHS} from './core/constants/routes.constants';
 import {predictionsRoutes} from './features/predictions/predictions.routes';
+import {userToolsRoutes} from './features/user-tools/user-tools.routes';
 
 export const routes: Routes = [
   ...dashboardRoutes,
   ...predictionsRoutes,
+  ...userToolsRoutes,
   { path: '', redirectTo: ROUTE_PATHS.DASHBOARD, pathMatch: 'full' },
 ];
