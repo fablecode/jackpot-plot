10176eebe390332b49b13e847d2b400503c115e6
fablecode <fablecode@users.noreply.github.com>
2025-04-25 17:14:43 +0200

    User tickets database tables created

---
+++ src/Infrastructure/JackpotPlot.Lottery.API.DatabaseMigration/Scripts/Migrations/2025/2025-04-25 - 0.0.0.1/Script0001 - Add_UserTickets_Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE user_tickets (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(255),
+    is_public BOOLEAN NOT NULL DEFAULT FALSE,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
---
+++ src/Infrastructure/JackpotPlot.Lottery.API.DatabaseMigration/Scripts/Migrations/2025/2025-04-25 - 0.0.0.1/Script0002 - Add_UserTicketPlays_Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE user_ticket_plays (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    ticket_id UUID NOT NULL REFERENCES user_tickets(id) ON DELETE CASCADE,
+    numbers INT[] NOT NULL,         -- e.g. ARRAY[5, 11, 19, 27, 34, 44]
+    line_index INT NOT NULL,             -- useful for ordering/position
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
