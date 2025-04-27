10fd82706fe61e9dc2974b60f6504e7d527e60b9
fablecode <fablecode@users.noreply.github.com>
2025-04-27 22:43:26 +0200

    Ticket domain extensions

---
+++ src/Application/JackpotPlot.Lottery.API.Application/Extensions/TicketDomainExtensions.cs
@@ -0,0 +1,12 @@
+﻿using JackpotPlot.Domain.Domain;
+using JackpotPlot.Lottery.API.Application.Models.Output;
+
+namespace JackpotPlot.Lottery.API.Application.Extensions;
+
+public static class TicketDomainExtensions
+{
+    public static TicketOutput ToOutput(this TicketDomain ticket)
+    {
+        return new TicketOutput(ticket.Id, ticket.Name, ticket.IsPublic);
+    }
+}
