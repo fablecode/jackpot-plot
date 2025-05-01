cb8e72e73ec5e49389eea8021dddf5dbac1bf4ed
fablecode <fablecode@users.noreply.github.com>
2025-05-01 10:14:09 +0200

    Unused using statements

--- src/Application/JackpotPlot.Prediction.API.Application/Features/PredictNext/PredictNextResponse.cs
+++ src/Application/JackpotPlot.Prediction.API.Application/Features/PredictNext/PredictNextResponse.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections.Immutable;
-using JackpotPlot.Domain.Domain;
-using JackpotPlot.Domain.Models;
-using JackpotPlot.Prediction.API.Application.Models.Output;
-using MediatR;
+﻿using JackpotPlot.Prediction.API.Application.Models.Output;
+using System.Collections.Immutable;
 
 namespace JackpotPlot.Prediction.API.Application.Features.PredictNext;
 
-public record PredictNextResponse(int LotteryId, int NumberOfPlays, string Strategy, ImmutableArray<PredictionOutput> Predictions) : IRequest<Result<PredictionDomain>>;
+public record PredictNextResponse(int LotteryId, int NumberOfPlays, string Strategy, ImmutableArray<PlayOutput> Plays);
