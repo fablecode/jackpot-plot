149c045370069a09dcee235eff35b58cb803f2ea
fablecode <fablecode@users.noreply.github.com>
2025-05-03 15:55:39 +0200

    Can create an empty ticket without plays

--- src/Application/JackpotPlot.Lottery.API.Application/Models/Input/CreateTicketInput.cs
+++ src/Application/JackpotPlot.Lottery.API.Application/Models/Input/CreateTicketInput.cs
@@ -1,3 +1,13 @@
 ﻿namespace JackpotPlot.Lottery.API.Application.Models.Input;
 
-public record CreateTicketInput(string Name, CreateTicketPlaysInput[] Plays);
+public record CreateTicketInput
+{
+public string Name { get; init; }
+public CreateTicketPlaysInput[] Plays { get; init; } = [];
+
+public CreateTicketInput(string name, CreateTicketPlaysInput[]? plays = null)
+{
+    Name = name;
+    Plays = plays ?? [];
+}
+}
