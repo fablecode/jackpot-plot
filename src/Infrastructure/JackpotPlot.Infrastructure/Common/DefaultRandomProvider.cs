a4b50148eea58dc2306487cf567d2453b6e8b23d
fablecode <fablecode@users.noreply.github.com>
2025-10-09 21:48:51 +0200

    Random number provider

---
+++ src/Infrastructure/JackpotPlot.Infrastructure/Common/DefaultRandomProvider.cs
@@ -0,0 +1,8 @@
+﻿using JackpotPlot.Application.Abstractions.Common;
+
+namespace JackpotPlot.Infrastructure.Common;
+
+public sealed class DefaultRandomProvider : IRandomProvider
+{
+    public Random Get() => new();
+}
