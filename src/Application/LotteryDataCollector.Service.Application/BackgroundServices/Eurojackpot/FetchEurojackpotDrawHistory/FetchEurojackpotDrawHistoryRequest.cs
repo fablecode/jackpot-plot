dc43ac8fc18b96da538e086687b71f4900eee74c
fablecode <fablecode@users.noreply.github.com>
2025-10-09 21:33:10 +0200

    LotterDataCollector.Service.Application deleted.

--- src/Application/LotteryDataCollector.Service.Application/BackgroundServices/Eurojackpot/FetchEurojackpotDrawHistory/FetchEurojackpotDrawHistoryRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace LotteryDataCollector.Service.Application.BackgroundServices.Eurojackpot.FetchEurojackpotDrawHistory;
-
-public record FetchEurojackpotDrawHistoryRequest : IRequest;
