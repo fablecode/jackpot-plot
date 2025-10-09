773c8f9317870d817794128fa61150ba665404c3
fablecode <fablecode@users.noreply.github.com>
2025-10-09 21:40:10 +0200

    Pure domain driven design, Prediction strategy deleted replaced with algorithms instead.

--- src/Domain/JackpotPlot.Domain/Services/IEurojackpotService.cs
+++
@@ -1,9 +0,0 @@
-﻿using JackpotPlot.Domain.Models;
-
-namespace JackpotPlot.Domain.Services;
-
-public interface IEurojackpotService
-{
-    IEnumerable<EurojackpotResult> GetAllDrawHistoryResults();
-    IAsyncEnumerable<EurojackpotResult> GetAllDrawHistoryResultsAsync();
-}
