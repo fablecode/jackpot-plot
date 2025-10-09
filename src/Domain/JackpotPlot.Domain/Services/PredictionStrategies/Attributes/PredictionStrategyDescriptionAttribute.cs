084550ca1bcc22f04a08b8c7234a6a4aaf0de3ee
fablecode <fablecode@users.noreply.github.com>
2025-10-09 21:37:55 +0200

    Moved and renamed to PredictionAlgorithmDescriptionAttribute

--- src/Domain/JackpotPlot.Domain/Services/PredictionStrategies/Attributes/PredictionStrategyDescriptionAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JackpotPlot.Domain.Services.PredictionStrategies.Attributes;
-
-[AttributeUsage(AttributeTargets.Class, Inherited = false)]
-public sealed class PredictionStrategyDescriptionAttribute(string id, string description) : Attribute
-{
-    public string Id { get; init; } = id;
-    public string Description { get; init; } = description;
-}
