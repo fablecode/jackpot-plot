1a4a4c1b15a0cbf1f8028c5d00c6301a76599874
fablecode <fablecode@users.noreply.github.com>
2025-10-10 20:12:16 +0200

    Message processors deleted

--- src/Application/JackpotPlot.Application.Abstractions/Messaging/Processors/IMessageProcessor.cs
+++
@@ -1,8 +0,0 @@
-﻿using JackpotPlot.Domain.Models;
-
-namespace JackpotPlot.Application.Abstractions.Messaging.Processors;
-
-public interface IMessageProcessor<T>
-{
-    Task<Result<T>> ProcessAsync(T message, CancellationToken cancellationToken);
-}
