397568073d2feba19117e71f1c4028a9ab7a5e8e
fablecode <fablecode@users.noreply.github.com>
2025-10-15 21:11:55 +0200

    Cancellation token added.

--- src/Application/JackpotPlot.Application.Abstractions/Services/IEurojackpotService.cs
+++ src/Application/JackpotPlot.Application.Abstractions/Services/IEurojackpotService.cs
@@ -1,9 +1,10 @@
 ﻿using JackpotPlot.Domain.Models;
+using System.Runtime.CompilerServices;
 
 namespace JackpotPlot.Application.Abstractions.Services;
 
 public interface IEurojackpotService
 {
     IEnumerable<EurojackpotResult> GetAllDrawHistoryResults();
-    IAsyncEnumerable<EurojackpotResult> GetAllDrawHistoryResultsAsync();
+    IAsyncEnumerable<EurojackpotResult> GetAllDrawHistoryResultsAsync(CancellationToken cancellationToken = default);
 }
