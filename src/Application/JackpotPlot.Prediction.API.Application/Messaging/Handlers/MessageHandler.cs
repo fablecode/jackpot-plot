0df8fa7899b54edbb5cd971945d2d79949df68f5
fablecode <fablecode@users.noreply.github.com>
2025-10-22 18:18:17 +0200

    Moved to infrastructure

--- src/Application/JackpotPlot.Prediction.API.Application/Messaging/Handlers/MessageHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿using JackpotPlot.Domain.Models;
-using MediatR;
-
-namespace JackpotPlot.Prediction.API.Application.Messaging.Handlers;
-
-public record MessageHandler<T> (T Message) : IRequest<Result<T>>;
