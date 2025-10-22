a1a1cd28e5779653f8b3a78e2573c7f76c524558
fablecode <fablecode@users.noreply.github.com>
2025-10-22 18:20:13 +0200

    IOptions<RabbitMqSettings> dependency now used for ExchangeName

--- src/Infrastructure/JackpotPlot.Infrastructure/Messaging/MassTransitExchangeWriter.cs
+++ src/Infrastructure/JackpotPlot.Infrastructure/Messaging/MassTransitExchangeWriter.cs
@@ -1,19 +1,26 @@
 ﻿using JackpotPlot.Application.Abstractions.Messaging;
+using JackpotPlot.Domain.Settings;
 using MassTransit;
+using Microsoft.Extensions.Options;
 
 namespace JackpotPlot.Infrastructure.Messaging;
 
 public sealed class MassTransitExchangeWriter<T> : IQueueWriter<T>
 {
     private readonly IBus _bus;
-    private static readonly Uri Exchange = new("exchange:lottery-exchange?type=topic&durable=true");
+    private readonly IOptions<RabbitMqSettings> _options;
 
-    public MassTransitExchangeWriter(IBus bus) => _bus = bus;
+    public MassTransitExchangeWriter(IBus bus, IOptions<RabbitMqSettings> options)
+    {
+        _bus = bus;
+        _options = options;
+    }
 
     public async Task Publish(T message, string routingKey, CancellationToken ct = default)
     {
-        var ep = await _bus.GetSendEndpoint(Exchange);
-        await ep.Send(message, ctx => ctx.SetRoutingKey(routingKey), ct);
+        var ep = await _bus.GetSendEndpoint(new($"exchange:{_options.Value.Exchange}?type=topic&durable=true"));
+
+        await ep.Send(message ?? throw new ArgumentNullException(nameof(message)), ctx => ctx.SetRoutingKey(routingKey), ct);
     }
 }
 
