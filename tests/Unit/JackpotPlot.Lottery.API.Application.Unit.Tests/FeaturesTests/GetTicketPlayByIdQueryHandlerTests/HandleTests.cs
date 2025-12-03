47cb5de032e4527d8170c526449453032ca094fc
fablecode <fablecode@users.noreply.github.com>
2025-12-03 18:25:48 +0100

    GetTicketPlayByIdQueryHandler Unit Tests

---
+++ tests/Unit/JackpotPlot.Lottery.API.Application.Unit.Tests/FeaturesTests/GetTicketPlayByIdQueryHandlerTests/HandleTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using JackpotPlot.Lottery.API.Application.Features.GetTicketPlayById;
+using NUnit.Framework;
+
+namespace JackpotPlot.Lottery.API.Application.Unit.Tests.FeaturesTests.GetTicketPlayByIdQueryHandlerTests;
+
+[TestFixture]
+public class HandleTests
+{
+    private GetTicketPlayByIdQueryHandler _sut;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _sut = new GetTicketPlayByIdQueryHandler();
+    }
+
+    [Test]
+    public async Task Given_Valid_Request_When_Handle_Is_Invoked_Should_Throw_NotImplementedException()
+    {
+        // Arrange
+        var request = new GetTicketPlayByIdQuery(Guid.NewGuid());
+
+        // Act
+        Func<Task> act = async () => await _sut.Handle(request, CancellationToken.None);
+
+        // Assert
+        await act.Should().ThrowAsync<NotImplementedException>();
+    }
+}
